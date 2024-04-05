92f89a48ccf2cf72e2f26059ec599bad67c51401
jordan <jordan@fiftyfootfoghorn.com>
2024-04-05 13:26:38 -0700

    add `ask`, rename to `gen`, tidy

--- gpt.py
+++ gpt.py
@@ -2,6 +2,7 @@ import openai
 from IPython.core.magic import (Magics, magics_class, line_magic)
 from prompt_toolkit import print_formatted_text
 from prompt_toolkit.formatted_text import PygmentsTokens
+from pygments.lexers.markup import MarkdownLexer
 from pygments.lexers.python import PythonLexer
 from pygments import lex
 
@@ -11,52 +12,83 @@ from prompt_toolkit.shortcuts import confirm
 @magics_class
 class GPT(Magics):
 
-    @line_magic
-    def gpt(self, line):
-        lexer = PythonLexer()
-
-        sys_message = {
-            "role": "system",
-            "content": "You are a python codebot, running in an ipython session. Respond ONLY in python source code. Do not use markdown."
-        }
-        history = self.shell.history_manager.get_range()
-        concatenated_history = '\n'.join([item[2] for item in history])
+    def _sample(self, line, system_message) -> str:
+        history = self._history_lines()
         client = openai.OpenAI()
         response = client.chat.completions.create(
             model="gpt-4-turbo-preview",
             messages=[
-                sys_message,
-                {"role": "user", "content": concatenated_history},
+                system_message,
+                {"role": "user", "content": history},
                 {"role": "user", "content": line},
             ],
             max_tokens=2000,
             stream=True,
         )
-        full_response = ""
         current_line = ""
         for chunk in response:
             if chunk.choices:
                 c = chunk.choices[0].delta.content
                 if c:
-                    full_response += c
                     current_line += c
                     if "\n" in current_line:
-                        print_highlighted_line(current_line, lexer)
+                        yield current_line
                         current_line = ""
         if current_line:
-            print_highlighted_line(current_line, lexer)
-        print("\n---")
+            yield current_line
+
+    def _history_lines(self):
+        history = self.shell.history_manager.get_range(output=True)
+        history_lines = []
+        for session, line_number, input_output in history:
+            inp, out = input_output
+            history_lines.append(f"In [{line_number}]: {inp}")
+            if out is not None:
+                history_lines.append(f"Out[{line_number}]: {str(out)}\n")
+            else:
+                history_lines.append("\n")
+        return '\n'.join(history_lines)
+
+    @line_magic
+    def ask(self, line):
+        lexer = MarkdownLexer()
+        sys_message = {
+            "role": "system",
+            "content": """
+            You are a python coding assistant, running in an ipython session.
+            Be concise and tailor your answers for a staff level engineer.
+            You may use markdown.
+            """
+        }
+        for sampled_line in self._sample(line, sys_message):
+            print_lexed(sampled_line, lexer)
+
+    @line_magic
+    def gen(self, line):
+        lexer = PythonLexer()
+        sys_message = {
+            "role": "system",
+            "content": """
+            You are a python codebot, running in an ipython session.
+            Respond ONLY in python source code. Do not use markdown.
+            """
+        }
+        full_response = ""
+        for sampled_line in self._sample(line, sys_message):
+            full_response += sampled_line
+            print_lexed(sampled_line, lexer)
+        print("\n")
         if confirm("Run it?"):
             code = trim_markdown(full_response)
             self.shell.run_cell(code)
 
 
-def print_highlighted_line(line: str, lexer: PythonLexer) -> None:
+def print_lexed(line: str, lexer) -> None:
     tokens = list(lex(line, lexer))
     print_formatted_text(PygmentsTokens(tokens), end="")
 
 
-def trim_markdown(input: str) -> str:
+def trim_markdown(inp: str) -> str:
     def delete_prefix(prefix, text):
         if text.startswith(prefix):
             return text[len(prefix):]
@@ -67,4 +99,4 @@ def trim_markdown(input: str) -> str:
             return text[:-len(suffix)]
         return text
 
-    return delete_prefix("python", delete_prefix("```", delete_suffix("```", input.strip().rstrip())))
+    return delete_prefix("python", delete_prefix("```", delete_suffix("```", inp.strip().rstrip())))
